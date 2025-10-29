6a9b99c0c896507d8c716cbbc0081d040faa3f90
Rina <foxytealblue@gmail.com>
2025-10-29 01:22:36 -0500

    Updated index.html and rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,55 @@
-console.log("Hello World!");
+function getRandomNumber(parameter) {
+  return Math.floor(Math.random() * parameter);
+}
+
+function getComputerChoice(number) {
+  switch (number) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
+}
+
+function getPlayerChoice() {
+  let isValidInput = false;
+
+  while (!isValidInput) {
+    let choice = prompt(
+      "Please choose Rock, Paper, or Scissors",
+    )?.toLowerCase();
+    console.log(choice);
+    switch (choice) {
+      case "rock":
+        isValidInput = true;
+        return "Rock";
+      case "paper":
+        isValidInput = true;
+        return "Paper";
+      case "scissors":
+        isValidInput = true;
+        return "Scissors";
+      case undefined:
+        isValidInput = true;
+        break;
+      default:
+        alert("Incorrect response.  Please try again.");
+    }
+  }
+}
+
+function playerScore(point) {
+  let playerScore = playerScore + point;
+  return playerScore;
+}
+
+function computerScore(point) {
+  let computerScore = computerScore + point;
+  return computerScore;
+}
+
+console.log(getComputerChoice(getRandomNumber(3)));
+
+console.log(`Player choice is ${getPlayerChoice()}`);
