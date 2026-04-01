796b22196bd778d55d99a724d5bfa15939f833ce
Rina <foxytealblue@gmail.com>
2026-03-31 23:14:28 -0500

    updated index.html and rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,6 @@
 const gameButtons = document.querySelectorAll(".gameButton");
+let playerScore = 0;
+let aiScore = 0;
 
 gameButtons.forEach((gameButton) =>
   gameButton.addEventListener("click", runGame),
@@ -7,7 +9,11 @@ gameButtons.forEach((gameButton) =>
 function runGame(event) {
   const playerChoice = event.target.id;
   const aiChoice = getAiChoice();
-  console.log(`${playerChoice} vs ${aiChoice}`);
+  setVsFrame(event, aiChoice);
+  const roundWinner = getRoundWinner(playerChoice, aiChoice);
+  scoreKeeper(roundWinner);
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`AI Score: ${aiScore}`);
 }
 
 function getAiChoice() {
@@ -21,4 +27,74 @@ function getAiChoice() {
   }
 }
 
-function roundWinner(playerChoice, aiChoice) {}
+function getRoundWinner(playerChoice, aiChoice) {
+  if (playerChoice === "rock") {
+    switch (aiChoice) {
+      case "Rock":
+        return "Tie";
+      case "Paper":
+        return "AI";
+      case "Scissors":
+        return "Player";
+    }
+  }
+  if (playerChoice === "paper") {
+    switch (aiChoice) {
+      case "Rock":
+        return "Player";
+      case "Paper":
+        return "Tie";
+      case "Scissors":
+        return "AI";
+    }
+  }
+  if (playerChoice === "scissors") {
+    switch (aiChoice) {
+      case "Rock":
+        return "AI";
+      case "Paper":
+        return "Player";
+      case "Scissors":
+        return "Tie";
+    }
+  }
+}
+
+function scoreKeeper(roundWinner) {
+  if (roundWinner === "Tie") {
+    return;
+  }
+  return roundWinner === "Player" ? playerScore++ : aiScore++;
+}
+
+function setVsFrame(event, aiChoice) {
+  const playerFrame = document.querySelector("#playerChoice");
+  const aiFrame = document.querySelector("#aiChoice");
+  const playerImage = getComputedStyle(event.currentTarget).backgroundImage;
+  const aiImage = getAiImage(aiChoice);
+
+  playerFrame.style.height = "494px";
+  playerFrame.style.width = "360px";
+  playerFrame.style.backgroundImage = playerImage;
+  playerFrame.style.backgroundSize = "cover";
+  playerFrame.style.backgroundPosition = "center";
+  playerFrame.style.backgroundRepeat = "no-repeat";
+  aiFrame.style.height = "494px";
+  aiFrame.style.width = "360px";
+  aiFrame.style.backgroundImage = `url("${aiImage}")`;
+  aiFrame.style.backgroundSize = "cover";
+  aiFrame.style.backgroundPosition = "center";
+  aiFrame.style.backgroundRepeat = "no-repeat";
+}
+
+function getAiImage(aiChoice) {
+  if (aiChoice === "Rock") {
+    return "./images/the-rock.webp";
+  }
+  if (aiChoice === "Paper") {
+    return "./images/toilet-paper-man.png";
+  }
+  if (aiChoice === "Scissors") {
+    return "./images/brooding-scissors.png";
+  }
+}
