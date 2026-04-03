2610c6e59e42f74b4b4916f10d96a9704f8f7352
Rina <foxytealblue@gmail.com>
2026-04-03 17:57:52 -0500

    updated index.html, style.css and rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,22 @@
 const gameButtons = document.querySelectorAll(".gameButton");
 let playerScore = 0;
 let aiScore = 0;
+let roundCount = 0;
 
 gameButtons.forEach((gameButton) =>
   gameButton.addEventListener("click", runGame),
 );
 
 function runGame(event) {
-  const playerChoice = event.target.id;
+  const playerChoice = event.currentTarget.id;
   const aiChoice = getAiChoice();
-  setVsFrame(event, aiChoice);
+  hideRPSContainer();
+  showVsFrame(event, aiChoice);
   const roundWinner = getRoundWinner(playerChoice, aiChoice);
+  roundCount++;
   scoreKeeper(roundWinner);
+  displayWinner(roundWinner);
+  displayScore();
   console.log(`Player Score: ${playerScore}`);
   console.log(`AI Score: ${aiScore}`);
 }
@@ -67,12 +72,27 @@ function scoreKeeper(roundWinner) {
   return roundWinner === "Player" ? playerScore++ : aiScore++;
 }
 
+function showVsFrame(event, aiChoice) {
+  const footer = document.querySelector("footer");
+
+  footer.style.opacity = "1";
+  footer.style.visibility = "visible";
+  setVsFrame(event, aiChoice);
+}
+
 function setVsFrame(event, aiChoice) {
   const playerFrame = document.querySelector("#playerChoice");
   const aiFrame = document.querySelector("#aiChoice");
+  const vsFrame = document.querySelector("#vsFrame");
   const playerImage = getComputedStyle(event.currentTarget).backgroundImage;
   const aiImage = getAiImage(aiChoice);
 
+  vsFrame.style.backgroundImage = "url('./images/vs-letters-versus-png.png')";
+  vsFrame.style.height = "262.5px";
+  vsFrame.style.width = "175px";
+  vsFrame.style.backgroundSize = "cover";
+  vsFrame.style.backgroundPosition = "center";
+  vsFrame.style.backgroundRepeat = "no-repeat";
   playerFrame.style.height = "494px";
   playerFrame.style.width = "360px";
   playerFrame.style.backgroundImage = playerImage;
@@ -97,4 +117,32 @@ function getAiImage(aiChoice) {
   if (aiChoice === "Scissors") {
     return "./images/brooding-scissors.png";
   }
+}
+
+function displayWinner(roundWinner) {
+  const announceWinner = document.querySelector("#announceWinner");
+
+  announceWinner.textContent =
+    roundWinner === "Tie" ? "It's a Tie!" : `The Winner is: ${roundWinner}!`;
+}
+
+function displayScore() {
+  const scoreHeader = document.querySelector("#scoreHeader");
+  const showPlayerScore = document.querySelector("#showPlayerScore");
+  const showAiScore = document.querySelector("#showAiScore");
+
+  scoreHeader.textContent = "Score";
+  showPlayerScore.textContent = `Player: ${playerScore}`;
+  showAiScore.textContent = `AI: ${aiScore}`;
+}
+
+function hideRPSContainer() {
+  const rpsContainer = document.querySelector("#rps-container");
+
+  rpsContainer.style.opacity = "0";
+  rpsContainer.style.visibility = "hidden";
+  rpsContainer.addEventListener("transitionend", function handler() {
+    rpsContainer.style.display = "none";
+    rpsContainer.removeEventListener("transitionend", handler);
+  });
 }
